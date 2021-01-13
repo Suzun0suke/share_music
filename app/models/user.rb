c529faeea1980d165c2e2451ce51e05537462e5e
Suzun0suke <76026243+Suzun0suke@users.noreply.github.com>
2021-01-13 15:27:12 +0900

    投稿機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :image
+  has_many :posts
+
   with_options presence: true do
   validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message:"半角英数字を含む6文字以上のパスワードにしてください"}
   validates :name, uniqueness: { case_sensitive: false }
   end
+
 end
