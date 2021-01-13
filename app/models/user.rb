9bee9e95e0987e9c88aa144f08d8bdb6b3e7f462
Suzun0suke <76026243+Suzun0suke@users.noreply.github.com>
2021-01-13 11:43:30 +0900

    ユーザー機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :image
+  with_options presence: true do
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message:"半角英数字を含む6文字以上のパスワードにしてください"}
+  validates :name, uniqueness: true
+  end
 end
