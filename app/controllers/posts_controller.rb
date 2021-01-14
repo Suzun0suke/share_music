19aa3bc1c4e758dea6ad7cdfa794908e4a81aece
Suzun0suke <76026243+Suzun0suke@users.noreply.github.com>
2021-01-14 15:59:57 +0900

    spotfy情報取得

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each do |post|
+    sp_list
+    end
   end
   def new
     @post = Post.new
@@ -18,5 +21,14 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :url, :image).merge(user_id: current_user.id)
+  end
+  def sp_list
+    url = "https://open.spotify.com/playlist/3OPacWA7p63EXLCDYMO2rO"
+    url.slice!("https://open.spotify.com/playlist/")
+    require 'rspotify'
+    RSpotify.authenticate(ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"])
+    playlist = RSpotify::Playlist.find_by_id(url)
+    @musics = playlist.tracks(limit:10)
+    @musics_num = @musics.length
   end
 end
