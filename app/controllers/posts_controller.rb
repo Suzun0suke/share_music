f4d7e830e974e3046d23e157e054e5302ca30dc1
Suzun0suke <76026243+Suzun0suke@users.noreply.github.com>
2021-01-14 19:30:32 +0900

    spotify読み込み

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,13 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all
-    @posts.each do |post|
-    sp_list
-    end
   end
+
   def new
     @post = Post.new
   end
+
   def create
     @post = Post.new(post_params)
     if @post.save
@@ -17,18 +17,25 @@ class PostsController < ApplicationController
     end
   end
 
+  def sp_list(url)
+    musics =[]
+    url.slice!("https://open.spotify.com/playlist/")
+    require 'rspotify'
+    RSpotify.authenticate(ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"])
+    playlist = RSpotify::Playlist.find_by_id(url)
+    music_list = playlist.tracks(limit:10)
+    music_list.length.times do |i|
+      musics << music_list[i]
+    end
+    return musics
+  end
+
+  helper_method :sp_list
+
   private
 
   def post_params
     params.require(:post).permit(:title, :url, :image).merge(user_id: current_user.id)
   end
-  def sp_list
-    url = "https://open.spotify.com/playlist/3OPacWA7p63EXLCDYMO2rO"
-    url.slice!("https://open.spotify.com/playlist/")
-    require 'rspotify'
-    RSpotify.authenticate(ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"])
-    playlist = RSpotify::Playlist.find_by_id(url)
-    @musics = playlist.tracks(limit:10)
-    @musics_num = @musics.length
-  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  
+
   with_options presence: true do
     validates :title
     validates :url
   end
+  
 end
