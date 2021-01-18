e9aa521b2763511854618efbfb10189b79a74648
Suzun0suke <76026243+Suzun0suke@users.noreply.github.com>
2021-01-18 15:13:10 +0900

    タグ機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:"posts#index"
-  resources :posts, only:[:new, :create] 
+  resources :posts, only:[:new, :create] do
+    collection do
+      get 'search'
+    end
+  end
 end
