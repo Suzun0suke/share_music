378179ae0043bb368630925b2ea0f8b7dd130674
Suzun0suke <76026243+Suzun0suke@users.noreply.github.com>
2021-01-21 16:13:33 +0900

    投稿削除機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :image
-  has_many :posts
+  has_many :posts, dependent: :destroy
 
   with_options presence: true do
   validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message:"半角英数字を含む6文字以上のパスワードにしてください"}
