1dfaa2f0c5ecef88f59c622a11bb2239d42d71a6
Suzun0suke <76026243+Suzun0suke@users.noreply.github.com>
2021-01-28 11:32:17 +0900

    実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -48,38 +48,6 @@ class PostsController < ApplicationController
     @posts = Post.search(params[:keyword])
   end
 
-  def sp_list(url)
-    if url.match("https://open.spotify.com/playlist/")
-      musics =[]
-      url.slice!("https://open.spotify.com/playlist/")
-      require 'rspotify'
-      RSpotify.authenticate(ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"])
-      playlist = RSpotify::Playlist.find_by_id(url)
-      music_list = playlist.tracks(limit:10)
-      music_list.length.times do |i|
-        musics << music_list[i]
-      end
-      return musics
-    end
-  end
-
-  def sp_list_full(url)
-    if url.match("https://open.spotify.com/playlist/")
-      musics =[]
-      url.slice!("https://open.spotify.com/playlist/")
-      require 'rspotify'
-      RSpotify.authenticate(ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"])
-      playlist = RSpotify::Playlist.find_by_id(url)
-      music_list = playlist.tracks
-      music_list.length.times do |i|
-        musics << music_list[i]
-        end
-        return musics
-      end
-    end
-
-  helper_method :sp_list, :sp_list_full
-
   private
 
   def post_params
