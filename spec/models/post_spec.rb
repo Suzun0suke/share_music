a98a130354fe84072573d869261ef56e180f9a61
Suzun0suke <76026243+Suzun0suke@users.noreply.github.com>
2021-02-01 15:36:07 +0900

    テスト追加

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "投稿" do
+    before do
+      @post = FactoryBot.build(:post)
+    end
+
+    context "投稿が保存できる" do
+      it "必要な情報がある" do
+        expect(@post).to be_valid
+      end
+      it "ハッシュタグがない" do 
+        @post.tag_list = nil
+        expect(@post).to be_valid
+      end
+    end
+
+    context "投稿が保存できない" do
+      it "タイトルがない" do
+        @post.title = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Title can't be blank")
+      end
+      it "URLがない" do
+        @post.url = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Url can't be blank")
+      end
+      it "画像がない" do
+        @post.image = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Image can't be blank")
+      end
+    end
+  end
+  
 end
