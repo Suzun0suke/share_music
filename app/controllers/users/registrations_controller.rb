ee84042cc2c7748d0789356b0dcd5204c435b6cf
Suzun0suke <76026243+Suzun0suke@users.noreply.github.com>
2021-02-17 14:05:26 +0900

    プロフィール編集

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,10 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_account_update_params, only: [:update]
+
+  protected
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image])
+  end
+
+end
